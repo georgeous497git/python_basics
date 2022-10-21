4a0dee99cfd4e117f522e98e0af6145f0cefd14c
georgeous497 <georgeous497@gmail.com>
2022-10-20 21:10:42 -0500

    First code

---
+++ printing.py
@@ -0,0 +1,11 @@
+print("First Python code.")
+
+# input keyword is waiting for a keyboard input
+name = input("What's your name?: ")
+
+age = input("How old are you?: ")
+
+print(f'You typed: (Name = {name}, Age = {age})')
+
+
+
