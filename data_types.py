792145d63472421ffe9c9f85af69b77060d8a5d3
georgeous497 <georgeous497@gmail.com>
2022-10-20 21:20:16 -0500

    Variables type | String, Int, Boolean

---
+++ data_types.py
@@ -0,0 +1,18 @@
+# string
+text = "text"
+text = 'text more text'
+print('Type String: ', text)
+# To know what's the variable type
+print(type(text))
+
+# int
+number = 2022
+print('Type Int: ', number)
+# To know what's the variable type
+print(type(number))
+
+# boolean
+flag = True  # With uppercase True || False
+print('Type Boolean: ', flag)
+# To know what's the variable type
+print(type(flag))
