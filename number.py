5c52e40c4b3b7f9dad0e6d3ace85fef6d1f6b681
georgeous497 <georgeous497@gmail.com>
2022-10-20 21:38:29 -0500

    Numbers

---
+++ number.py
@@ -0,0 +1,24 @@
+# int
+age = 35
+print(type(age))
+# float
+temperature = 24.5
+print(type(temperature))
+
+# decremental
+age -= 1
+print(age)
+# incrementation
+age += 2
+print(age)
+
+#########################################
+
+january_budget = 27500
+february_budget = 29000
+march_budget = 25000
+addition = january_budget + february_budget + march_budget
+
+budget_average = addition / 3
+
+print(f'Your budget average is: {budget_average}')
