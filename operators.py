6804e4b3ad07228becd8e662003ca8ba1946e9ab
georgeous497 <georgeous497@gmail.com>
2022-10-24 20:52:19 -0500

    Arithmetic Operators

---
+++ operators.py
@@ -0,0 +1,9 @@
+num1 = 10.5
+num2 = 200
+print('Addition:' + str(num1 + num2))
+print('Subtraction:' + str(num1 - num2))
+print('Multiplication:' + str(num1 * num2))
+print('Division:' + str(num1 / num2))
+print('Module:' + str(num1 % num2))
+print('Integers from division:' + str(num1 // num2))
+print('Pow:' + str(num1 ** num2))
