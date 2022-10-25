11e14e72128f0150c9ffa0cc3aafacdc7a978d0a
georgeous497 <georgeous497@gmail.com>
2022-10-24 21:54:03 -0500

    if elif else

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+print("-= IF operator =-")
+
+name = input("Tell me a mexican F1 pilot name: ")
+
+if name == "Checo":
+    print('You are correct Chequito!')
+elif name == "Sergio":
+    print('You are correct Serch!')
+else:
+    print('You failed!')
