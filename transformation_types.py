bbdaae5108357d55654246e9b6c8aeb81cd68b0c
georgeous497 <georgeous497@gmail.com>
2022-10-24 20:44:27 -0500

    Transformation of types (Casting)

---
+++ transformation_types.py
@@ -0,0 +1,16 @@
+name = 'Jorge'
+print(f'Name: {name}')
+print(type(name))
+name = 35
+print(f'Name: {name} ????')
+print(type(name))
+print("===============================")
+pets = 5
+text = "I have " + str(pets) + " pets"
+print(text)
+print("===============================")
+dogs = "3"
+cats = "5"
+pets = int(3) + int(5)
+text = "I have " + str(pets) + " pets"
+print(text)
